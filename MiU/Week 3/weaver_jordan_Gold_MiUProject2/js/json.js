0b31e8b220ae6d816cc845d50aacf8875bd0cbc2
Jordan Weaver <weaver.jordan6@gmail.com>
2013-09-15 17:00:20 -0500

    Meaningfull Commit (Fixed all problems from Week2, except Thumbnail Grid and Edit function)

---
+++ MiU/Week 3/weaver_jordan_Gold_MiUProject2/js/json.js
@@ -0,0 +1,164 @@
+// JavaScript Document
+
+var json = {
+	"goal1": {
+		"gname": ["Goal Name: ", "Savings"],
+		"date": ["Date of Achievment: ", "2013-11-13"],
+		"typeGoal": ["Type of Goal: ", "Savings"],
+		"schedule": ["Goal Activity: ", "Weekly"],
+		"amount": ["Amount Saved: ", "120"],
+		"comments":["Other Comments: ", "Other Notes"]
+	},
+	"goal2": {
+		"gname": ["Goal Name: ", "Study"],
+		"date": ["Date of Achievment: ", "2013-12-11"],
+		"typeGoal": ["Type of Goal: ", "Education"],
+		"schedule": ["Goal Activity: ", "Daily"],
+		"amount": ["Amount Saved: ", "90"],
+		"comments":["Other Comments: ", "90 pages per day"]
+	},
+		"goal3": {
+		"gname": ["Goal Name: ", "iPhone"],
+		"date": ["Date of Achievment: ", "2013-09-10"],
+		"typeGoal": ["Type of Goal: ", "Savings"],
+		"schedule": ["Goal Activity: ", "Weekly"],
+		"amount": ["Amount Saved: ", "200"],
+		"comments":["Other Comments: ", "iPhone 5S"]
+	},
+		"goal4": {
+		"gname": ["Goal Name: ", "Get Abs"],
+		"date": ["Date of Achievment: ", "2134-05-11"],
+		"typeGoal": ["Type of Goal: ", "Workout"],
+		"schedule": ["Goal Activity: ", "Daily"],
+		"amount": ["Amount Saved: ", "30"],
+		"comments":["Other Comments: ", "30 sit ups a day, to get abs for the babes"]
+	},
+		"goal5": {
+		"gname": ["Goal Name: ", "Eat Healthy"],
+		"date": ["Date of Achievment: ", "2013-09-20"],
+		"typeGoal": ["Type of Goal: ", "Personal"],
+		"schedule": ["Goal Activity: ", "Daily"],
+		"amount": ["Amount Saved: ", "2,400"],
+		"comments":["Other Comments: ", "2,400 Calories a day."]
+	},
+	"goal6": {
+		"gname": ["Goal Name: ", "Save Lots of Money"],
+		"date": ["Date of Achievment: ", "2014-01-01"],
+		"typeGoal": ["Type of Goal: ", "Personal"],
+		"schedule": ["Goal Activity: ", "Monthly"],
+		"amount": ["Amount Saved: ", "1,000"],
+		"comments":["Other Comments: ", "Save up for loans."]
+	},
+	"goal7": {
+		"gname": ["Goal Name: ", "Move into New House"],
+		"date": ["Date of Achievment: ", "2013-09-07"],
+		"typeGoal": ["Type of Goal: ", "Personal"],
+		"schedule": ["Goal Activity: ", ""],
+		"amount": ["Amount Saved: ", ""],
+		"comments":["Other Comments: ", "Be Fully moved in by Saturday."]
+	},
+	"goal8": {
+		"gname": ["Goal Name: ", "Homework Homework Homework"],
+		"date": ["Date of Achievment: ", "2013-09-30"],
+		"typeGoal": ["Type of Goal: ", "Education"],
+		"schedule": ["Goal Activity: ", "Daily"],
+		"amount": ["Amount Saved: ", "4"],
+		"comments":["Other Comments: ", "Put in 4 hours a day for this course."]
+	},
+	"goal9": {
+		"gname": ["Goal Name: ", "Sell cars"],
+		"date": ["Date of Achievment: ", "2013-09-07"],
+		"typeGoal": ["Type of Goal: ", "Personal"],
+		"schedule": ["Goal Activity: ", ""],
+		"amount": ["Amount Saved: ", "4"],
+		"comments":["Other Comments: ", "Sell 4 cars at the shop."]
+	},
+	"goal10": {
+		"gname": ["Goal Name: ", "Finish this Project"],
+		"date": ["Date of Achievment: ", "2013-09-05"],
+		"typeGoal": ["Type of Goal: ", "Education"],
+		"schedule": ["Goal Activity: ", ""],
+		"amount": ["Amount Saved: ", ""],
+		"comments":["Other Comments: ", "Have a Fully functioning app and meet requirments."]
+	},
+	"goal11": {
+		"gname": ["Goal Name: ", "Figure out 9 more Goals"],
+		"date": ["Date of Achievment: ", "2013-09-04"],
+		"typeGoal": ["Type of Goal: ", "Education"],
+		"schedule": ["Goal Activity: ", ""],
+		"amount": ["Amount Saved: ", "9"],
+		"comments":["Other Comments: ", "I realized I have boring life goals."]
+	},
+	"goal12": {
+		"gname": ["Goal Name: ", "Wake up on Time"],
+		"date": ["Date of Achievment: ", "2013-09-05"],
+		"typeGoal": ["Type of Goal: ", "Personal"],
+		"schedule": ["Goal Activity: ", "Daily"],
+		"amount": ["Amount Saved: ", ""],
+		"comments":["Other Comments: ", "I've been doing a good job. Make sure to keep it up till Friday."]
+	},
+	"goal13": {
+		"gname": ["Goal Name: ", "Solve Global Hunger"],
+		"date": ["Date of Achievment: ", "2013-10-31"],
+		"typeGoal": ["Type of Goal: ", "Workout"],
+		"schedule": ["Goal Activity: ", ""],
+		"amount": ["Amount Saved: ", ""],
+		"comments":["Other Comments: ", "I'll get right on that."]
+	},
+	"goal14": {
+		"gname": ["Goal Name: ", "Keep up Vegan Lifestyle"],
+		"date": ["Date of Achievment: ", "2013-12-30"],
+		"typeGoal": ["Type of Goal: ", "Personal"],
+		"schedule": ["Goal Activity: ", "Daily"],
+		"amount": ["Amount Saved: ", ""],
+		"comments":["Other Comments: ", "Vegan life or die."]
+	},
+	"goal15": {
+		"gname": ["Goal Name: ", "Skate"],
+		"date": ["Date of Achievment: ", "2013-09-24"],
+		"typeGoal": ["Type of Goal: ", "Workout"],
+		"schedule": ["Goal Activity: ", "Weekly"],
+		"amount": ["Amount Saved: ", "100"],
+		"comments":["Other Comments: ", "Skate a 100 hours a week, and I'll be an expert in 100 weeks."]
+	},
+	"goal16": {
+		"gname": ["Goal Name: ", "Sleep More"],
+		"date": ["Date of Achievment: ", "2013-09-11"],
+		"typeGoal": ["Type of Goal: ", "Personal"],
+		"schedule": ["Goal Activity: ", "Daily"],
+		"amount": ["Amount Saved: ", "8"],
+		"comments":["Other Comments: ", "I barely sleep and need much more since I lack protein (just kidding)."]
+	},
+	"goal17": {
+		"gname": ["Goal Name: ", "Eat Thamine Mononitrate"],
+		"date": ["Date of Achievment: ", "2013-09-29"],
+		"typeGoal": ["Type of Goal: ", "Personal"],
+		"schedule": ["Goal Activity: ", ""],
+		"amount": ["Amount Saved: ", "0"],
+		"comments":["Other Comments: ", "It's a migraine trigger and found primarly in grains. Also known as B1"]
+	},
+	"goal18": {
+		"gname": ["Goal Name: ", "Save for Acupunture"],
+		"date": ["Date of Achievment: ", "2013-09-10"],
+		"typeGoal": ["Type of Goal: ", "Savings"],
+		"schedule": ["Goal Activity: ", "Weekly"],
+		"amount": ["Amount Saved: ", "90"],
+		"comments":["Other Comments: ", "Acupunture has been known to help migraine"]
+	},
+	"goal19": {
+		"gname": ["Goal Name: ", "Save for Show"],
+		"date": ["Date of Achievment: ", "2013-09-14"],
+		"typeGoal": ["Type of Goal: ", "Savings"],
+		"schedule": ["Goal Activity: ", "Weekly"],
+		"amount": ["Amount Saved: ", "100"],
+		"comments":["Other Comments: ", "Saves The Day and Hostage Calm in Iowa City"]
+	},
+	"goal20": {
+		"gname": ["Goal Name: ", "New pair of shoes"],
+		"date": ["Date of Achievment: ", "2013-10-10"],
+		"typeGoal": ["Type of Goal: ", "Savings"],
+		"schedule": ["Goal Activity: ", ""],
+		"amount": ["Amount Saved: ", "60"],
+		"comments":["Other Comments: ", "Vans or Nike"]
+	},
+}
